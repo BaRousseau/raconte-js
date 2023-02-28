0336c263e40ef93940c3e1d3addd19eee17cd65f
ROUSSEAU Bastien <brousseau@apside.fr>
2023-02-28 14:55:33 +0100

    feat: Final content at the end of README

---
+++ index.js
@@ -0,0 +1,7 @@
+window.onload = function () {
+    // Je déclare une variable et je lie une string 'coucou' à cette variable
+    var maVariable = 'coucou';
+
+    console.log('Je suis un ' + maVariable + ' de la jungle JavaScript.');
+    console.log(document.getElementById('main'));
+}
